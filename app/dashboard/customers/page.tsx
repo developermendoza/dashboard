8642adb9624b0b1ebe340582a3b7eed631ef3801
Jose Mendoza <developermendoza@gmail.com>
2023-11-20 21:22:52 -0800

    add customers table and fetch customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,36 @@
-export default function Page() {
-  return <div>Customers Page</div>;
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/customers/table';
+// import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { lusitana } from '@/app/ui/fonts';
+// import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+// import { Suspense } from 'react';
+// import { fetchInvoicesPages } from '@/app/lib/data';
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  return (
+    <div className="w-full">
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        {/* <Search placeholder="Search customers..." /> */}
+        {/* <CreateInvoice /> */}
+      </div>
+      {/* <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense> */}
+
+      <Table query={query} />
+      <div className="mt-5 flex w-full justify-center">
+        {/* <Pagination totalPages={totalPages} /> */}
+      </div>
+    </div>
+  );
 }
